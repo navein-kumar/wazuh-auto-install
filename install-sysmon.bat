a5f7824c98fe90815b758b1a2ff64b6a93a3dc5b
Naveen-Kumar <30574883+navein-kumar@users.noreply.github.com>
2025-09-18 15:43:35 +0530

    Update install-sysmon.bat

--- install-sysmon.bat
+++ install-sysmon.bat
@@ -5,11 +5,12 @@ REM Check admin rights
 net session >nul 2>&1 || (echo Run as Administrator! & pause & exit)
 
 REM Download files
-powershell -Command "Invoke-WebRequest -Uri 'https://github.com/navein-kumar/wazuh_sysmon_new/raw/refs/heads/main/Sysmon64.exe' -OutFile 'Sysmon64.exe'"
-powershell -Command "Invoke-WebRequest -Uri 'https://github.com/navein-kumar/wazuh_sysmon_new/raw/refs/heads/main/windows_sysmon_config.xml' -OutFile 'windows_sysmon_config.xml'"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/navein-kumar/wazuh_sysmon_new/refs/heads/main/Sysmon64.exe' -OutFile 'Sysmon64.exe'"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/navein-kumar/wazuh_sysmon_new/refs/heads/main/windows_sysmon_config.xml' -OutFile 'windows_sysmon_config.xml'"
 
 REM Install Sysmon
 .\Sysmon64.exe -accepteula -i windows_sysmon_config.xml
 
 echo Done!
 pause
+
